7c77b234a20fe392f5cb27f82334dd67d627fd3d
Joshua Hart <joshua.hart@protonmail.com>
2021-03-07 20:59:44 +0000

    change div height for full black background

--- vega-front-end/src/App.js
+++ vega-front-end/src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Leaderboard from './Leaderboard'
 function App() {
   return (
-    <div style={{ backgroundColor: 'black', height: '100vh', padding: '5rem' }}>
+    <div style={{ backgroundColor: 'black', height: '100%', padding: '5rem' }}>
       <div
         className='App'
         style={{
